20c9429f5dd49a31fa3564d48c99aae6ac35250e
cwhuang29 <hq678835@gmail.com>
2023-01-11 10:14:43 -0800

    Revise content script

--- src/content/Highlighter/scripts.js
+++ src/content/Highlighter/scripts.js
@@ -1,17 +1,27 @@
 export const insertIcons = () => {
-  const fontStyle = document.createElement('link');
+  const links = ['https://fonts.googleapis.com/icon?family=Material+Icons'];
+  const id = 'vocabulary-highlighter-icon-';
 
-  fontStyle.id = 'vocabulary-highlighter-icon';
-  fontStyle.rel = 'stylesheet';
-  fontStyle.href = 'https://fonts.googleapis.com/icon?family=Material+Icons';
-  document.head.appendChild(fontStyle);
+  links.forEach((link, idx) => {
+    const fontStyle = document.createElement('link');
+
+    fontStyle.id = `${id}-${idx}`;
+    fontStyle.rel = 'stylesheet';
+    fontStyle.href = link;
+    document.head.appendChild(fontStyle);
+  });
 };
 
 export const insertFontStyles = () => {
-  const fontStyle = document.createElement('link');
+  const links = ['https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'];
+  const id = 'vocabulary-highlighter-font-style-';
 
-  fontStyle.id = 'vocabulary-highlighter-font-style';
-  fontStyle.rel = 'stylesheet';
-  fontStyle.href = 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap';
-  document.head.appendChild(fontStyle);
+  links.forEach((link, idx) => {
+    const fontStyle = document.createElement('link');
+
+    fontStyle.id = `${id}-${idx}`;
+    fontStyle.rel = 'stylesheet';
+    fontStyle.href = link;
+    document.head.appendChild(fontStyle);
+  });
 };
