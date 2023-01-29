a8afb86613f75a0d4bdc510aef1554b2e4e2cb2a
cwhuang29 <hq678835@gmail.com>
2023-01-28 16:26:16 -0800

    Revise eslint settings

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
-
 import { crx } from '@crxjs/vite-plugin';
 import react from '@vitejs/plugin-react';
 
 import path from 'path';
+import { defineConfig } from 'vite';
+
 import manifest from './manifest.json';
 
 // https://vitejs.dev/config/
